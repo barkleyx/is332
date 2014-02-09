c9774c4a89f6032afbb2848b548eeced97523db8
Barkley Xu unknown student <bx6@afsconnect1.njit.edu>
2014-02-09 17:32:51 -0500

    Added check if mobile block

--- js/main.js
+++ js/main.js
@@ -22,3 +22,17 @@
 		sh = document.body.clientHeight;
 		checkMobile();
 	});
+	
+	//Check if Mobile
+	function checkMobile() {
+		mobile = (sw > breakpoint) ? false : true;
+		if (!mobile) { //If Not Mobile
+			loadAux();
+			$('.aux header a').addClass('disabled').addClass('open');
+			$('[role="tabpanel"],#nav,#search').show(); //Show full navigation and search
+		} else { //Hide 
+			if(!$('#nav-anchors a').hasClass('active')) {
+				$('#nav,#search').hide();
+			}
+		}
+	}
